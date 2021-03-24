136990e1106b08320098b992c45bd6ca863b9f14
Davoleo <davoleo2001@gmail.com>
2021-03-24 14:01:32 +0100

    feat(structure): Glass Types Enum

---
+++ src/main/java/net/davoleo/crystalglass/block/GlassType.java
@@ -0,0 +1,13 @@
+package net.davoleo.crystalglass.block;
+
+public enum GlassType {
+    NORMAL,
+    CLEAR,
+    BRIGHT,
+    BRIGHT_CLEAR,
+    ETHEREAL_PLAYER,
+    ETHEREAL_ENTITIES,
+    OPAQUE,
+    UNBREAKABLE,
+    ;
+}
