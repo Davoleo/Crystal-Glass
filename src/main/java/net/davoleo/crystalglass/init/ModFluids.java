248ed033ad69c36ca1ab28001dea8f5b0a64e599
Davoleo <dav@davoleo.net>
2022-08-22 17:33:22 +0200

    Fix fluid being weird - fluidblock link was missing

--- src/main/java/net/davoleo/crystalglass/init/ModFluids.java
+++ src/main/java/net/davoleo/crystalglass/init/ModFluids.java
@@ -2,6 +2,7 @@ package net.davoleo.crystalglass.init;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraftforge.fluids.FluidAttributes;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,14 +17,14 @@ public final class ModFluids extends ModRegistry {
     public static final ResourceLocation FLOWING_TEXTURE = resourceLoc("fluid/molten_crystal_flow");
     public static final ResourceLocation OVERLAY_TEXTURE = resourceLoc("fluid/molten_crystal_overlay");
 
-    public static final RegistryObject<ForgeFlowingFluid> MOLTEN_CRYSTAL = FLUIDS.register("molten_crystal",
+    public static final RegistryObject<FlowingFluid> MOLTEN_CRYSTAL = FLUIDS.register("molten_crystal",
             () -> new ForgeFlowingFluid.Source(ModFluids.MOLTEN_CRYSTAL_PROPS));
 
-    public static final RegistryObject<ForgeFlowingFluid> MOLTEN_CRYSTAL_FLOWING = FLUIDS.register("molten_crystal_flowing",
+    public static final RegistryObject<FlowingFluid> MOLTEN_CRYSTAL_FLOWING = FLUIDS.register("molten_crystal_flowing",
             () -> new ForgeFlowingFluid.Flowing(ModFluids.MOLTEN_CRYSTAL_PROPS));
 
     public static final ForgeFlowingFluid.Properties MOLTEN_CRYSTAL_PROPS = new ForgeFlowingFluid.Properties(
-            () -> MOLTEN_CRYSTAL.get(), () -> MOLTEN_CRYSTAL_FLOWING.get(),
+            MOLTEN_CRYSTAL, MOLTEN_CRYSTAL_FLOWING,
             FluidAttributes.builder(STILL_TEXTURE, FLOWING_TEXTURE)
                     .density(15)
                     .luminosity(2)
@@ -33,7 +34,9 @@ public final class ModFluids extends ModRegistry {
                     .color(0xBFFFFFFF)
                     .temperature(1000)
                     .sound(SoundEvents.BUCKET_FILL_LAVA, SoundEvents.BUCKET_EMPTY_LAVA)
-    ).slopeFindDistance(4).levelDecreasePerBlock(2).bucket(ModItems.MOLTEN_CRYSTAL_BUCKET);
+    ).slopeFindDistance(4).levelDecreasePerBlock(2)
+            .block(ModBlocks.MOLTEN_CRYSTAL_BLOCK)
+            .bucket(ModItems.MOLTEN_CRYSTAL_BUCKET);
 
 
 }
--- src/main/java/net/davoleo/crystalglass/init/ModRegistry.java
+++ src/main/java/net/davoleo/crystalglass/init/ModRegistry.java
@@ -5,6 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,9 +18,11 @@ public sealed class ModRegistry permits ModBlocks, ModFluids, ModItems {
 
     public static void init()
     {
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        FLUIDS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        BLOCKS.register(eventBus);
+        ITEMS.register(eventBus);
+        FLUIDS.register(eventBus);
 
         ModBlocks.init();
         ModItems.init();
